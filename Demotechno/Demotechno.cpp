634ba7d7bca2aee54ef18a32dce0d6a05c7a0c00
Marcel Michel <m.michel.10.97@gmail.com>
2022-09-16 12:46:02 -0400

    Commit 5

--- Demotechno/Demotechno.cpp
+++ Demotechno/Demotechno.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello programmeurs chez Stim!\n";
+    std::cout << "Hello programmeurs chez Stim dans la branche master!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
